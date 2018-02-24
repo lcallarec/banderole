a21134cacd39929b613327686ffcf7ee0b8793da
Laurent Callarec <l.callarec@gmail.com>
2018-02-24 11:03:12 +0100

    Feature object is immutable from calling side

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,15 +1,16 @@
 const banderole = require('../index');
+const testData = require('./feature-flags.json');
 
-const featureFlags = require('./feature-flags.json');
-
-banderole.boot(featureFlags);
+beforeEach(() => {
+    banderole.boot(testData);
+});
 
 describe('A feature-flag can be defined by a boolean value', () => {
-  test('A feature-flag explicitely set to false should be disabled', () => {
+  test('A feature-flag explicitely set to true should be enabled', () => {
     expect(banderole.isEnabled('switchboard')).toBeTruthy();
   });
 
-  test('A feature-flag explicitely set to true should be enabled', () => {
+  test('A feature-flag explicitely set to false should be disabled', () => {
     expect(banderole.isEnabled('clock')).toBeFalsy();
   });
 });
@@ -27,3 +28,17 @@ describe('A feature-flag can be defined by an object containing an enabled key',
         expect(banderole.isEnabled('slack-integration')).toBeFalsy();
     });
 });
+
+test('The internal feature-flags object should not be muted is the original is mutated ', () => {
+    
+    //Given
+    let featureFlags = {features: {}};
+    banderole.boot(featureFlags);
+
+    //When
+    featureFlags["features"]["i_shouldnt_be_there"] = true;
+
+    //Then
+    expect(banderole.isEnabled('i_shouldnt_be_there')).toBeFalsy();
+});
+
--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ let featureFlags = {
   features: {},
 };
 
+let context = {};
+
 const boot = (features) => {
-  featureFlags = features;
+  featureFlags.features = {...features.features};
 };
 
 const isEnabled = (feature) => {
+
   const flag = featureFlags['features'][feature];
 
   if (typeof flag === 'boolean') {
     return flag;
   }
 
-  if (typeof flag === 'object' && flag.hasOwnProperty("enabled")) {
+  if (typeof flag === 'object' && flag.hasOwnProperty('enabled')) {
       return flag.enabled;
   }
 
